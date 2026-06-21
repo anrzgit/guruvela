69493de1400a680cb1ba12910c357a43ded1cdfc
aditya-detect <aditya.k@detecttechnologies.com>
2026-06-21 14:37:56 +0530

    supabase fix

--- guruvela_app/android/app/build.gradle.kts
+++ guruvela_app/android/app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 4
-        versionName = "1.4.0"
+        versionCode = 5
+        versionName = "1.5.0"
     }
 
     signingConfigs {
